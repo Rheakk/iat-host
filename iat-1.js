47d339f6b1f1813124a319f4b7eae6a4fb9fc52f
Rheakk <36390761+Rheakk@users.noreply.github.com>
2025-09-19 13:18:08 -0700

    Update iat-1.js

--- iat-1.js
+++ iat-1.js
@@ -3,19 +3,19 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
+			name : 'Asian people', //Will appear in the data.
 			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
+				media : {image : 'Asian people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    {image : 'asian_face_1.jpg'}, 
+    			{image : 'asian_face_2.jpg'}, 
+    			{image : 'asian_face_3.jpg'}, 
+    			{image : 'asian_face_4.jpg'}, 
+    			{image : 'asian_face_5.jpg'}, 
+    			{image : 'asian_face_6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -23,23 +23,63 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 		category2 :	{
 			name : 'White people', //Will appear in the data.
 			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
+				media : {image : 'White people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'yf1.jpg'}, 
-    			{image : 'yf4.jpg'}, 
-    			{image : 'yf5.jpg'}, 
-    			{image : 'ym2.jpg'}, 
-    			{image : 'ym3.jpg'}, 
-    			{image : 'ym5.jpg'}			], 
+    		    {image : 'ea_face_1.jpg'}, 
+    			{image : 'ea_face_2.jpg'}, 
+    			{image : 'ea_face_3.jpg'}, 
+    			{image : 'ea_face_4.jpg'}, 
+    			{image : 'ea_face_5.jpg'}, 
+    			{image : 'ea_face_6.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
+		category3 :	{
+			name : 'Good words', //Will appear in the data.
+			title : {
+				media : {word : 'Good words'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word: 'Celebrate'},
+				{word: 'Magnificent'},
+				{word: 'Fabulous'},
+				{word: 'Beautiful'},
+				{word: 'Lovely'},
+				{word: 'Glad'},
+				{word: 'Spectacular'},
+				{word: 'Terrific'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		category4 :	{
+			name : 'Bad words', //Will appear in the data.
+			title : {
+				media : {word : 'Bad words'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word: 'Negative'},
+				{word: 'Angry'},
+				{word: 'Poison'},
+				{word: 'Pain'},
+				{word: 'Humiliate'},
+				{word: 'Sadness'},
+				{word: 'Awful'},
+				{word: 'Scorn'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		}
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://Rheakk.github.io/iat-host/images/'
 		} 
 	});
 });
