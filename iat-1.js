57b0ca9c8c42f9ff151301f4d94284441e92639f
Rheakk <36390761+Rheakk@users.noreply.github.com>
2025-09-19 11:59:04 -0700

    Update iat-1.js

--- iat-1.js
+++ iat-1.js
@@ -1,21 +1,21 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
 		category1 : {
-			name : 'Asian people', //Will appear in the data.
+			name : 'Black people', //Will appear in the data.
 			title : {
-				media : {word : 'Asian people'}, //Name of the category presented in the task.
+				media : {word : 'Black people'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'asian_face_1.jpg'}, 
-    			{image : 'asian_face_2.jpg'}, 
-    			{image : 'asian_face_3.jpg'}, 
-    			{image : 'asian_face_4.jpg'}, 
-    			{image : 'asian_face_5.jpg'}, 
-    			{image : 'asian_face_6.jpg'}
+    		    {image : 'black1.jpg'}, 
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -28,18 +28,18 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'ea_face_1.jpg'}, 
-    			{image : 'ea_face_2.jpg'}, 
-    			{image : 'ea_face_3.jpg'}, 
-    			{image : 'ea_face_4.jpg'}, 
-    			{image : 'ea_face_5.jpg'}, 
-    			{image : 'ea_face_6.jpg'}			], 
+    		    {image : 'yf1.jpg'}, 
+    			{image : 'yf4.jpg'}, 
+    			{image : 'yf5.jpg'}, 
+    			{image : 'ym2.jpg'}, 
+    			{image : 'ym3.jpg'}, 
+    			{image : 'ym5.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://rheakk.github.io/iat-host/images/'
+			image : 'https://baranan.github.io/minno-tasks/images/'
 		} 
 	});
 });
