7d0cdee8747c31bb4c70147940c620ce403156c5
Rheakk <36390761+Rheakk@users.noreply.github.com>
2025-09-19 13:34:46 -0700

    Update iat-1.js

--- iat-1.js
+++ iat-1.js
@@ -1,85 +1,85 @@
 define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
-    var API = new APIConstructor();
+  var API = new APIConstructor();
 
-	return iatExtension({
-		category1 : {
-			name : 'Asian people', //Will appear in the data.
-			title : {
-				media : {image : 'Asian people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'asian_face_1.jpg'}, 
-    			{image : 'asian_face_2.jpg'}, 
-    			{image : 'asian_face_3.jpg'}, 
-    			{image : 'asian_face_4.jpg'}, 
-    			{image : 'asian_face_5.jpg'}, 
-    			{image : 'asian_face_6.jpg'}
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'White people', //Will appear in the data.
-			title : {
-				media : {image : 'White people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'ea_face_1.jpg'}, 
-    			{image : 'ea_face_2.jpg'}, 
-    			{image : 'ea_face_3.jpg'}, 
-    			{image : 'ea_face_4.jpg'}, 
-    			{image : 'ea_face_5.jpg'}, 
-    			{image : 'ea_face_6.jpg'}			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		attribute1 :	{
-			name : 'Good words', //Will appear in the data.
-			title : {
-				media : {word : 'Good words'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {word: 'Celebrate'},
-				{word: 'Magnificent'},
-				{word: 'Fabulous'},
-				{word: 'Beautiful'},
-				{word: 'Lovely'},
-				{word: 'Glad'},
-				{word: 'Spectacular'},
-				{word: 'Terrific'}
-			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},
-		attribute2 :	{
-			name : 'Bad words', //Will appear in the data.
-			title : {
-				media : {word : 'Bad words'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {word: 'Negative'},
-				{word: 'Angry'},
-				{word: 'Poison'},
-				{word: 'Pain'},
-				{word: 'Humiliate'},
-				{word: 'Sadness'},
-				{word: 'Awful'},
-				{word: 'Scorn'}
-			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		}
+  return iatExtension({
+    category1 : {
+      name : 'Asian people',
+      title : {
+        media : {word : 'Asian people'},   // FIXED: word, not image
+        css : {color:'#31940F','font-size':'2em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {image : 'asian_face_1.jpg'},
+        {image : 'asian_face_2.jpg'},
+        {image : 'asian_face_3.jpg'},
+        {image : 'asian_face_4.jpg'},
+        {image : 'asian_face_5.jpg'},
+        {image : 'asian_face_6.jpg'}
+      ],
+      stimulusCss : {color:'#31940F','font-size':'1.8em'}
+    },
 
-		base_url : {//Where are your images at?
-			image : 'https://Rheakk.github.io/iat-host/images/'
-		} 
-	});
+    category2 : {
+      name : 'White people',
+      title : {
+        media : {word : 'White people'},   // FIXED: word, not image
+        css : {color:'#31940F','font-size':'2em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {image : 'ea_face_1.jpg'},
+        {image : 'ea_face_2.jpg'},
+        {image : 'ea_face_3.jpg'},
+        {image : 'ea_face_4.jpg'},
+        {image : 'ea_face_5.jpg'},
+        {image : 'ea_face_6.jpg'}
+      ],
+      stimulusCss : {color:'#31940F','font-size':'1.8em'}
+    },
+
+    attribute1 : {
+      name : 'Good words',
+      title : {
+        media : {word : 'Good words'},
+        css : {color:'#31940F','font-size':'2em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {word: 'Celebrate'},
+        {word: 'Magnificent'},
+        {word: 'Fabulous'},
+        {word: 'Beautiful'},
+        {word: 'Lovely'},
+        {word: 'Glad'},
+        {word: 'Spectacular'},
+        {word: 'Terrific'}
+      ],
+      stimulusCss : {color:'#31940F','font-size':'1.8em'}
+    },
+
+    attribute2 : {
+      name : 'Bad words',
+      title : {
+        media : {word : 'Bad words'},
+        css : {color:'#31940F','font-size':'2em'},
+        height : 4
+      },
+      stimulusMedia : [
+        {word: 'Negative'},
+        {word: 'Angry'},
+        {word: 'Poison'},
+        {word: 'Pain'},
+        {word: 'Humiliate'},
+        {word: 'Sadness'},
+        {word: 'Awful'},
+        {word: 'Scorn'}
+      ],
+      stimulusCss : {color:'#31940F','font-size':'1.8em'}
+    }, // FIXED: comma here
+
+    base_url : {
+      image : 'https://Rheakk.github.io/iat-host/images/'
+    }
+  });
 });
